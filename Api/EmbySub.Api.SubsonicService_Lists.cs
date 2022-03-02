d9d83c883d3178bbf1224db7a91021c94e8c58b8
Gabe <gabe@boxboat.com>
2022-03-02 09:34:26 -0800

    WIP - Explanatory comments.

--- Api/EmbySub.Api.SubsonicService_Lists.cs
+++ Api/EmbySub.Api.SubsonicService_Lists.cs
@@ -35,6 +35,7 @@ namespace EmbySub.Api
           string xmlString;
           String hrmraw;
 
+          // if login is NOT successful return an error....
           if (!hrm.IsSuccessStatusCode)
           {
             subReq.ItemElementName = EmbySub.ItemChoiceType.error;
@@ -46,6 +47,7 @@ namespace EmbySub.Api
             xmlString = Serializer<EmbySub.Response>.Serialize(subReq);
             return ResultFactory.GetResult(Request, xmlString, null);
           }
+          // otherwise it was successful so grab & store the auth token
           else
           {
             hrmraw = await hrm.Content.ReadAsStringAsync();
@@ -55,13 +57,14 @@ namespace EmbySub.Api
             c.DefaultRequestHeaders.Add("X-Emby-Token", u.GetProperty("AccesssToken").ToString());
           }
 
+          // we need the unique ID for the desired music library set in the plugin configuration
           String url = String.Format("http://localhost:{0}/emby/Items?IncludeItemTypes=Album&ExcludeItemTypes=Audio", Plugin.Instance.Configuration.LocalEmbyPort);
           HttpResponseMessage mes = await c.GetAsync(url);
           String raw = await mes.Content.ReadAsStringAsync();
           JsonDocument j = JsonDocument.Parse(raw);
           JsonElement allLibs = j.RootElement.GetProperty("Items");
           String s;
-          String musicLibId = String.Empty;;
+          String musicLibId = String.Empty;
 
           foreach (JsonElement lib in allLibs.EnumerateArray())
           {
@@ -73,6 +76,20 @@ namespace EmbySub.Api
             }
           }
 
+          // music library does not exist under configured name
+          if (String.IsNullOrEmpty(musicLibId))
+          {
+            subReq.ItemElementName = EmbySub.ItemChoiceType.error;
+            EmbySub.Error e = new EmbySub.Error();
+            e.code = 0;
+            e.message = "Music library does not exist";
+            subReq.Item = e;
+            subReq.version = SupportedSubsonicApiVersion;
+            xmlString = Serializer<EmbySub.Response>.Serialize(subReq);
+            return ResultFactory.GetResult(Request, xmlString, null);
+          }
+
+          // library exists so let's get all the artists that have albums first
           url = String.Format("http://localhost:{0}/emby/Items?ParentId={1}&IncludeItemTypes=Album&ExcludeItemTypes=Audio", Plugin.Instance.Configuration.LocalEmbyPort, musicLibId);
           mes = await c.GetAsync(url);
           raw = await mes.Content.ReadAsStringAsync();
@@ -82,6 +99,7 @@ namespace EmbySub.Api
           subReq.ItemElementName = EmbySub.ItemChoiceType.albumList;
           List<EmbySub.Child> albums = new List<EmbySub.Child>();
 
+          // now for each artist let's get a list of their albums....
           foreach (JsonElement artist in allArtists.EnumerateArray())
           {
             String artistId = artist.GetProperty("Id").ToString();
@@ -92,6 +110,7 @@ namespace EmbySub.Api
 
             JsonElement allAlbumsOfArtist = k.RootElement.GetProperty("Items");
 
+            // ....and then add each album to our list
             foreach (JsonElement album in allAlbumsOfArtist.EnumerateArray())
             {
               EmbySub.Child ch = new EmbySub.Child();
@@ -104,6 +123,8 @@ namespace EmbySub.Api
             }
           }
 
+
+          // lastly let's convert the above list and add to our Subsonic XML to return
           EmbySub.AlbumList al = new EmbySub.AlbumList();
           al.album = albums.ToArray();
           subReq.Item = al;
